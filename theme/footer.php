55c45885c4f1ee49e3afa4fd754c0bd2964de90c
Andrea Veri <averi@redhat.com>
2016-11-28 19:39:43 +0100

    wgo is now hosted by Red Hat

--- theme/footer.php
+++ theme/footer.php
@@ -41,7 +41,7 @@
                 <small>
                     <?php _e( 'Free to share and remix', 'grass' ); ?>: <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons CC-BY</a>.
                     <?php _e( 'Optimized for standards', 'grass' ); ?>.
-                    <?php _e( 'Hosted by', 'grass' ); ?> <a href="http://www.canonical.com/">Canonical</a>.
+                    <?php _e( 'Hosted by', 'grass' ); ?> <a href="http://www.redhat.com/">Red Hat</a>.
                     <?php _e( 'Powered by', 'grass' ); ?> <a href="http://www.wordpress.org">WordPress</a>.
                 </small>
             </div>
