7e25fc2ee54fa618a033f26e22237fe869943592
Tom Tryfonidis <tomtryf@gnome.org>
2017-02-02 20:05:01 +0200

    Make post images to use img-responsive CSS class

--- theme/functions.php
+++ theme/functions.php
@@ -519,6 +519,25 @@ function custom_mimes_types ($existing_mimes) {
 require_once('lib/wp-bootstrap-navwalker.php');
 
 /*
+ * Add img-responsive CSS class to all post images
+ */
+
+function bootstrap_responsive_images( $html ){
+  $classes = 'img-responsive'; // separated by spaces, e.g. 'img image-link'
+  // check if there are already classes assigned to the anchor
+  if ( preg_match('/<img.*? class="/', $html) ) {
+    $html = preg_replace('/(<img.*? class=".*?)(".*?\/>)/', '$1 ' . $classes . ' $2', $html);
+  } else {
+    $html = preg_replace('/(<img.*?)(\/>)/', '$1 class="' . $classes . '" $2', $html);
+  }
+  // remove dimensions from images, does not need it!
+  // Keep dimensions for now..
+  // $html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
+  return $html;
+}
+add_filter( 'the_content','bootstrap_responsive_images',10 );
+
+/*
  * GNOME Grass Customizer
  */
 
